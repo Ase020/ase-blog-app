51647104521cbc4d3961babab4126a9a9b66902c
felixNyalenda <felixasejunior@gmail.com>
2023-09-25 23:14:19 +0300

    theme build

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,34 @@
+"use client";
+
+import Image from "next/image";
 import styles from "./themeToggle.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  const { theme, toggle } = useContext(ThemeContext);
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark"
+          ? { left: 1, backgroundColor: "#fff" }
+          : { right: 1, backgroundColor: "#0f172a" }
+      }
+    >
+      <Image src="/moon.png" alt="moon" height={15} width={15} />
+      <div
+        className={styles.circle}
+        style={
+          theme === "dark"
+            ? { left: 1, backgroundColor: "#0f172a" }
+            : { right: 1, backgroundColor: "#fff" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="sun" height={15} width={15} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
