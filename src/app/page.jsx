0f76de25df61a16a4ceb941a87264b80bd29c999
felixNyalenda <felixasejunior@gmail.com>
2023-09-25 18:48:57 +0300

    init setup

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      Hello
+      <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1>{" "}
+      <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1>{" "}
+      <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1> <h1>Ase</h1>{" "}
+      <h1>Ase</h1> <h1>Ase</h1>{" "}
+    </div>
+  );
+}
