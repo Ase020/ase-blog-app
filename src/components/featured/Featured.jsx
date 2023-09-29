752f01981cc85af8970d1d4b587f80a35127642a
felixNyalenda <felixasejunior@gmail.com>
2023-09-30 00:52:46 +0300

    commit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
 import styles from "./featured.module.css";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, ase here!</b> Discover my stories and creative ideas.
+      </h1>
+
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="post" />
+        </div>
+        <div className={styles.textContainer}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
